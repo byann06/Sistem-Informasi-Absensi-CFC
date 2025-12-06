ef351d0e4618725787a5d63d5f7d124c98c83a7a
Abyan Ihza Pradipta <abyanihza20@gmail.com>
2025-12-07 00:58:28 +0700

    Create NavbarNav And About Us
    This Web For Community Cfc ABSENSIN

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle active
+const navBarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navBarNav.classList.toggle('active');
+};
+
+// klik di luar sidebar untuk menghilankan nav
+const hamburger = document.querySelector('#hamburger-menu')
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navBarNav.contains(e.target)) {
+        navBarNav.classList.remove('active');
+    }
+})
